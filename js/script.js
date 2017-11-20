fa7eb7412ee7180634558d84f0cb978713281623
Dorkys Ramos <dorkysramos@gmail.com>
2017-11-20 04:51:24 +0000

    updated website

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ $(document).ready(function(){
   });
 
 });
+
+$('textarea.expand').focus(function () {
+    $(this).animate({ height: "4em", width:'100%' }, 500);
+}).blur(function function_name(argument) {
+   $(this).animate({ height: "4em", width:'50%' }, 500);
+})
